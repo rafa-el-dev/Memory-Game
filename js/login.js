4bcc87e0f206186d6bc2e11440c1acb6c59e3ab9
rafa-el-dev <rafael.ribeiro.nasci@gmail.com>
2024-04-22 19:15:24 +0100

    (feat): added handlesubmit function and coments

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 const input = document.querySelector(".login-input");
 const button = document.querySelector(".login-button");
+const form = document.querySelector(".login-form");
 
+// Function to validate the input and enable the button
 const validateInput = ({target}) => {
   const value = target.value;
   if(value.length > 2) {
@@ -11,4 +13,12 @@ const validateInput = ({target}) => {
   button.setAttribute("disabled", "");
 }
 
-input.addEventListener("input", validateInput); 
+// Function to handle the form submit, store the username in local storage and redirect
+const handleSubmit = (event) => {
+  event.preventDefault();
+  localStorage.setItem("player", input.value);
+  window.location = "../index.html";
+}
+
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
